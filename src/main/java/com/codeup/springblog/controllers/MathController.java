53131daee97d308f3849b065005f3e398b794309
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-09-21 10:26:38 -0500

    add method of MathController

---
+++ src/main/java/com/codeup/springblog/controllers/MathController.java
@@ -0,0 +1,18 @@
+package com.codeup.springblog.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MathController {
+    @GetMapping("add/{number1}/and/{number2}")
+    @ResponseBody
+
+    public String add(@PathVariable int number1, @PathVariable int number2){
+        return String.valueOf(number1 + number2);
+    }
+
+}
