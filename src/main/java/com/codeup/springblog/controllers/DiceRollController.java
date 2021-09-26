7437615a07751dd036eae54d8955844095f2c2cf
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-09-26 17:47:19 -0500

    completed dice roll controller

--- src/main/java/com/codeup/springblog/controllers/DiceRollController.java
+++ src/main/java/com/codeup/springblog/controllers/DiceRollController.java
@@ -1,6 +1,10 @@
 package com.codeup.springblog.controllers;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Random;
 
 
 @Controller
@@ -8,6 +12,19 @@ public class DiceRollController {
 
     @GetMapping("/roll-dice")
     public String rollDice(){
+        return "roll-dice";
+    }
+
+    @GetMapping("/roll-dice/{n}")
+    public String rollGuess(@PathVariable int n, Model model){
+
+        Random rand = new Random();
+        int randomNumber = rand.nextInt((6-1) + 1) + 1;
+
+        model.addAttribute("diceRoll", randomNumber);
+        model.addAttribute("userGuess", n);
+        model.addAttribute("isCorrect", randomNumber == n);
+
         return "roll-dice";
     }
 }
