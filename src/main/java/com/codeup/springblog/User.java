829d1d3dcb23ad161b26d6670de5987b2982860c
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-09-28 15:24:48 -0500

    creating user class

--- src/main/java/com/codeup/springblog/User.java
+++ src/main/java/com/codeup/springblog/User.java
@@ -1,4 +1,29 @@
 package com.codeup.springblog;
 
+import com.codeup.springblog.models.Post;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
 public class User {
+
+    public User(){
+
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
+
+    private List<Post> posts;
 }
