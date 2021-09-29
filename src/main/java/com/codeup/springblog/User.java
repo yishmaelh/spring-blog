9fd9b8ed995e0ef386cbb9ff804672f223933b1b
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-09-28 19:34:56 -0500

    completed user class

--- src/main/java/com/codeup/springblog/User.java
+++ src/main/java/com/codeup/springblog/User.java
@@ -9,8 +9,7 @@ import java.util.List;
 @Table(name = "users")
 public class User {
 
-    public User(){
-
+    public User() {
     }
 
     @Id
@@ -26,4 +25,36 @@ public class User {
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
 
     private List<Post> posts;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
 }
